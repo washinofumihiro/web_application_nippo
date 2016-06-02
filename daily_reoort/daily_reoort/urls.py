32f60d0db43464245cfb962731f97cc578210f33
washinofumihiro <washino.fumihiro@tis.co.jp>
2016-06-02 15:49:36 +0900

    add webapplication task (daily report management)

---
+++ daily_reoort/daily_reoort/urls.py
@@ -0,0 +1,29 @@
+"""daily_reoort URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include   # ←, includeを追加
+from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^day/', include('day.urls', namespace='day')),   # ←ここを追加
+    url(r'^login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'day/login.html'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout',
+        {'template_name': 'day/logged_out.html'}),
+]
---
+++ daily_reoort/day/forms.py
@@ -0,0 +1,17 @@
+from django.forms import ModelForm
+from day.models import Book, Impression
+
+
+class ImpressionForm(ModelForm):
+    """感想のフォーム"""
+    class Meta:
+        model = Impression
+        fields = ('comment', )
+
+class BookForm(ModelForm):
+    """書籍のフォーム"""
+    class Meta:
+        model = Book
+        fields = ('name', 'publisher', 'page', )
+
+
