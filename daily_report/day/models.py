3c66cdadd6ddc89dcab624dce5f14f5157c2b3cf
washinofumihiro <washino.fumihiro@tis.co.jp>
2016-08-22 12:46:50 +0900

    fix for rename

--- daily_report/day/models.py
+++ daily_report/day/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from daily_reoort.daily_reoort import settings
+# from daily_report.daily_report import settings
 from django.contrib.auth import get_user
 
 #ユーザー情報
