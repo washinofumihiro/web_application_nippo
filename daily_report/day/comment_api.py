ecb59d75134ca430f8acb02df07006216221a050
washinofumihiro <washino.fumihiro@tis.co.jp>
2016-10-20 11:23:56 +0900

    added comment out to define

--- daily_report/day/comment_api.py
+++ daily_report/day/comment_api.py
@@ -6,33 +6,22 @@ from datetime import datetime
 
 
 def list(report_id):
-    # reports = Report.objects.all().order_by('id')
-    # print(reports.title)
-    # report = get_object_or_404(Report, pk=kwargs['report_id'])  # 親の書籍を読む
-    # print(report_id)
+    """
+    コメント一覧の表示
+    :param report_id:
+    :return:
+    """
     comment = Report.objects.all().prefetch_related("impressions").get(id=report_id).impressions.all()
-    # print(comment)
-    # comment = report.impressions.all().order_by('id')
     return comment
-#
-# class ImpressionList(ListView):
-#     """感想の一覧"""
-#     context_object_name='impressions'
-#     template_name='day/impression_list.html'
-#     paginate_by = 10  # １ページは最大2件ずつでページングする
-#
-#     # @login_required
-#     def get(self, request, *args, **kwargs):
-#         report = get_object_or_404(Report, pk=kwargs['report_id'])  # 親の書籍を読む
-#         impressions = report.impressions.all().order_by('id')   # 書籍の子供の、感想を読む
-#         self.object_list = impressions
-#
-#         context = self.get_context_data(object_list=self.object_list, report=report)
-#         return self.render_to_response(context)
 
 
-# 日報のフォーム指定
 def show(comment_id, login_user):
+    """
+    編集または表示するコメントを選択
+    :param comment_id:
+    :param login_user:
+    :return:
+    """
     if comment_id:   # report_id が指定されている (修正時)
         comment = get_object_or_404(Impression, pk=comment_id)
     else:         # report_id が指定されていない (追加時)
@@ -43,44 +32,29 @@ def show(comment_id, login_user):
 
 
 def edit(post_data, comment, report_id):
+    """
+    コメントの編集
+    :param post_data:
+    :param comment:
+    :param report_id:
+    :return:
+    """
     date_object = datetime.now()
     report = get_object_or_404(Report, pk=report_id)
-    form = ImpressionForm(post_data, instance=comment)  # POST された request データからフォームを作成
-    if form.is_valid():  # フォームのバリデーション
+    form = ImpressionForm(post_data, instance=comment)
+    if form.is_valid():
         comment = form.save(commit=False)
         comment.report = report
-        comment.commnet_time = datetime(*date_object.timetuple()[:6])
+        comment.comment_time = datetime(*date_object.timetuple()[:6])
         comment.save()
     return form
 
 
 def delete(comment_id):
+    """
+    コメントの削除
+    :param comment_id:
+    :return:
+    """
     comment = get_object_or_404(Impression, pk=comment_id)
     comment.delete()
-
-
-def impression_edit(request, report_id, impression_id=None):
-    """感想の編集"""
-    date_object = datetime.now()
-    report = get_object_or_404(Report, pk=report_id)  # 親の書籍を読む
-    if impression_id:   # impression_id が指定されている (修正時)
-        impression = get_object_or_404(Impression, pk=impression_id)
-    else:               # impression_id が指定されていない (追加時)
-        impression = Impression()
-        impression.comment_user = request.user.username
-
-    if request.method == 'POST':
-        form = ImpressionForm(request.POST, instance=impression)  # POST された request データからフォームを作成
-        if form.is_valid():    # フォームのバリデーション
-            impression = form.save(commit=False)
-            impression.report = report  # この感想の、親の書籍をセット
-            impression.comment_time = datetime(*date_object.timetuple()[:6])
-            impression.save()
-            return redirect('day:impression_list', report_id=report_id)
-    else:    # GET の時
-        form = ImpressionForm(instance=impression)  # impression インスタンスからフォームを作成
-
-    return render(request,
-                  'day/impression_edit.html',
-                  dict(form=form, report_id=report_id, impression_id=impression_id))
-
