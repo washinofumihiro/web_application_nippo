2d1692f8e2abb8ee480b0d22e4e5cb7886b945f1
washinofumihiro <washino.fumihiro@tis.co.jp>
2016-10-20 11:26:07 +0900

    added comment out to define

--- daily_report/day/forms.py
+++ daily_report/day/forms.py
@@ -4,43 +4,33 @@ from .models import Report, Impression, Question, AnswerQuestion
 
 
 class ImpressionForm(forms.ModelForm):
-    """感想のフォーム"""
+    """コメントのフォーム"""
     class Meta:
         model = Impression
         fields = ('comment_user', 'comment',)
 
 
 class ReportForm(forms.ModelForm):
-    """書籍のフォーム"""
+    """日報のフォーム"""
     class Meta:
         model = Report
-        # fields = ('name', 'publisher', )
-        # fields = ( 'user','title', 'content')
-        fields = ('user', 'title', 'content_Y', 'content_W', 'content_T')
-        # fields = ( 'user','title', 'content', 'user_login_time', 'user_post_time')
-        # fields = ('date', 'title', 'user',)
+        fields = ('user', 'title', 'content_Y', 'content_W', 'content_T',)
 
-
-# class QuestionLevelForm(forms.ModelForm):
-#     class Meta:
-#         model = QuestionLevel
-#         fields = ('question_level_1', 'question_level_2', 'question_level_3', 'question_level_4', 'question_level_5')
-#
 
 class QuestionForm(forms.ModelForm):
+    """質問のフォーム"""
     class Meta:
         model = Question
         fields = ('question_content',)
-        # fields = "__all__"
 
 
 class AnswerForm(forms.ModelForm):
+    """質問に対しての回答のフォーム"""
     class Meta:
         model = AnswerQuestion
         fields = ('answer',)
 
 
 class SearchForm(forms.Form):
-
+    """検索のフォーム"""
     Search = forms.CharField(max_length=255)
-    print('search')
