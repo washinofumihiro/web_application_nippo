5090b18f3d0cc9992b6ec840551ee49d5077c579
washinofumihiro <washino.fumihiro@tis.co.jp>
2016-10-20 11:27:37 +0900

    added comment out to define

--- daily_report/day/models.py
+++ daily_report/day/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Report(models.Model):
-    """書籍"""
+    """日報のモデル"""
     title = models.CharField('タイトル', max_length=255)
     content_Y = models.TextField('やったこと(Y)', blank=True)
     content_W = models.TextField('わかったこと(W)', blank=True)
@@ -18,6 +18,7 @@ class Report(models.Model):
 
 
 class Impression(models.Model):
+    """コメントのモデル"""
     report = models.ForeignKey(Report, verbose_name='日報', related_name='impressions')
     comment = models.TextField('コメント', blank=True)
     comment_user = models.CharField('コメント投稿者', max_length=255)
@@ -27,19 +28,8 @@ class Impression(models.Model):
         return self.comment
 
 
-# class QuestionLevel(models.Model):
-#     report = models.ForeignKey(Report, verbose_name='日報', related_name='levels')
-#     question_level_1 = models.TextField('レベル1', blank=True)
-#     question_level_2 = models.TextField('レベル2', blank=True)
-#     question_level_3 = models.TextField('レベル3', blank=True)
-#     question_level_4 = models.TextField('レベル4', blank=True)
-#     question_level_5 = models.TextField('レベル5', blank=True)
-#
-#     def __str__(self):
-#         return self.level
-
-
 class Question(models.Model):
+    """質問のモデル"""
     report = models.ForeignKey(Report, verbose_name='日報', related_name='questions')
     question_content = models.TextField('質問内容', blank=True)
 
@@ -48,6 +38,7 @@ class Question(models.Model):
 
 
 class AnswerQuestion(models.Model):
+    """質問に対しての回答のフォーム"""
     question = models.ForeignKey(Question, verbose_name='質問', related_name='answers')
     answer = models.TextField('質問に対しての回答', blank=True)
 
